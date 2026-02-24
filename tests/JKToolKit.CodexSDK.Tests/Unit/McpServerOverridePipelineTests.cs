b75631fbac64adca010872987a01b187693d6ec2
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-02-24 23:49:01 +0100

    Suppress CS0067 warning for unused test event

--- tests/JKToolKit.CodexSDK.Tests/Unit/McpServerOverridePipelineTests.cs
+++ tests/JKToolKit.CodexSDK.Tests/Unit/McpServerOverridePipelineTests.cs
@@ -195,7 +195,9 @@ public sealed class McpServerOverridePipelineTests
 
     private sealed class FakeJsonRpcConnection : IJsonRpcConnection
     {
+#pragma warning disable CS0067 // Event is part of the IJsonRpcConnection contract; tests don't need to raise it.
         public event Func<JsonRpcNotification, ValueTask>? OnNotification;
+#pragma warning restore CS0067
 
         public Func<JsonRpcRequest, ValueTask<JsonRpcResponse>>? OnServerRequest { get; set; }
 
